089a99ca2c4defdfa6fe605ad904cc9529017d74
Brian Christensen <brianchristensen@outlook.com>
2014-01-16 07:08:04 -0800

    added view change transitions

--- public/app/app.js
+++ public/app/app.js
@@ -1,4 +1,5 @@
-var app = angular.module('app', ['ngRoute', 
+var app = angular.module('app', ['ngRoute',
+                                 'ngAnimate',
                                  'app.controllers']);
 
 // routes
